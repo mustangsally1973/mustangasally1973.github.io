37082c6a7a8f14fb103362d5f0958205cba23e06
mustangsally1973 <osfadev@gmail.com>
2023-03-12 09:05:26 +0900

    update src

---
+++ data/study_record.js
@@ -0,0 +1,4 @@
+var this_week = "2023-03-06 - 2023-03-12";
+var this_week_total = 534;
+var daily_data = {"labels": ["2023-03-06", "2023-03-07", "2023-03-08", "2023-03-09", "2023-03-10", "2023-03-11", "2023-03-12"], "datasets": [{"label": "\u587e\u76ee\u6a19", "data": [60, 60, 90, 60, 120, 120, 120], "borderWidth": 1}, {"label": "\u587e", "data": [27, 31, 83, 33, 133, 184, 43], "borderWidth": 1}, {"label": "\u5b66\u6821", "data": [35, 16, 19, 18, 56, 0, 0], "borderWidth": 1}]};
+var subject_data = {"labels": ["\u7b97\u6570", "\u56fd\u8a9e", "\u7406\u79d1", "\u793e\u4f1a", "\u7dcf\u5408", "\u5b66\u6821\u306e\u5bbf\u984c"], "datasets": [{"data": [170, 86, 139, 139, 0, 144], "backgroundColor": ["#3F51B5", "#D50000", "#F4511E", "#0B8043", "#8E24AA", "#616161"], "weight": 100}]};
